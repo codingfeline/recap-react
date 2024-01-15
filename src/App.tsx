cc605bc935121aadcd1deba9a1af08d7e927a127
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-01-15 22:34:35 +0000

    add ExpenseFilter

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,20 @@ import Alert from './components/Alert'
 import Like from './components/Like'
 import Form from './components/Form'
 import ExpenseList from './components/ExpenseList'
+import ExpenseFilter from './components/ExpenseFilter'
 
 const App = () => {
   const [visible, setVisible] = useState(false)
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
     { id: 2, description: 'aaa', amount: 10, category: 'Utilities' },
-    { id: 3, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 3, description: 'aaa', amount: 40, category: 'Entertainment' },
     { id: 4, description: 'aaa', amount: 10, category: 'Utilities' },
   ])
+  const [selectedCategory, setSetselectedCategory] = useState('')
+  const visibleExpenses = selectedCategory
+    ? expenses.filter(e => e.category === selectedCategory)
+    : expenses
 
   const items = [
     'Thomson Rd',
@@ -26,8 +31,11 @@ const App = () => {
 
   return (
     <div className="container">
+      <div className="m-2">
+        <ExpenseFilter onSelectCategory={category => setSetselectedCategory(category)} />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={id => setExpenses(expenses.filter(e => e.id !== id))}
       />
       <Form />
